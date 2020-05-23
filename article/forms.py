1d24bea927addf9c62d8cdafce18bd190edbdfde
orientor <orientorrenovare@gmail.com>
2020-05-23 18:44:25 +0530

    Add drop-down menu and create more models

--- article/forms.py
+++ article/forms.py
@@ -3,4 +3,4 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ["title","content","article_image"]
+        fields = ["article_type", "title","content","article_image"]
--- article/models.py
+++ article/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 # Create your models here.
 
+CHOICES = (
+    ('lost','LOST'),
+    ('found', 'FOUND'),
+    ('request','REQUEST'),
+    ('sell','SELL'),
+    ('giveaway','GIVEAWAY'),
+)
 class Article(models.Model):
     author = models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name = "Author")
     title = models.CharField(max_length = 50,verbose_name = "Title")
     content = models.TextField()
+    article_type = models.CharField(max_length=15, choices=CHOICES, default= "LOST", verbose_name="Type")
     created_date = models.DateTimeField(auto_now_add=True,verbose_name="Created Date")
     article_image = models.FileField(blank = True,null = True,verbose_name="Image")
     def __str__(self):
@@ -12,6 +20,25 @@ class Article(models.Model):
     
     class Meta:
         ordering = ['-created_date']
+
+class LostArticle(Article):
+    found = models.BooleanField(default=False, verbose_name="Found")
+
+class FoundArticle(Article):
+    claimed = models.BooleanField(default=False, verbose_name="Claimed")
+
+class RequestArticle(Article):
+    approvals = models.ManyToManyField("auth.User",verbose_name="appprovals")
+
+class SellArticle(Article):
+    base_price = models.IntegerField(verbose_name="Baseprice")
+    claimed = models.BooleanField(default=False)
+    highest_claimed_price=models.IntegerField(default=None)
+    highest_claimant  = models.ForeignKey("auth.User",on_delete=models.PROTECT,verbose_name="Hclaim")
+    deadline =models.DateTimeField(verbose_name = "deadline")
+class GiveAwayArticle(Article):
+    approvals = models.ManyToManyField("auth.User",verbose_name="appprovals")
+    deadline =models.DateTimeField()
 class Comment(models.Model):
     article = models.ForeignKey(Article,on_delete = models.CASCADE,verbose_name = "Article",related_name="comments")
     comment_author = models.CharField(max_length = 50,verbose_name = "Author")
