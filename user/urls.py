f334c74e9742009a9ad36f761ab705efffef5ccf
the-code-meister <snehal14201@gmail.com>
2020-05-24 00:48:41 +0400

    navbar and dashboard changed

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register/',views.register,name = "register"),
     path('login/',views.loginUser,name = "login"),
     path('logout/',views.logoutUser,name = "logout"),
+    path('settings/',views.settingUser,name = "settings"),
 
 ]
--- user/views.py
+++ user/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from .forms import RegisterForm,LoginForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth import login,authenticate,logout,update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 
@@ -54,5 +55,22 @@ def loginUser(request):
 def logoutUser(request):
     logout(request)
     messages.success(request,"Successfully logged out")
+    return redirect("index")
+
+
+def settingUser(request):
+    if request.user.is_authenticated:
+        form = None
+        if request.method == "POST":
+            form=PasswordChangeForm(request.user, request.POST)
+            if form.is_valid():
+                # user = form.save()
+                update_session_auth_hash(request,form.request.user)
+        user_info = User(user=request.user)
+        context = { 'user_info' : user_info,
+                    'form' : form }
+        messages.success(request,"Password successfully changed")
+        return render(request,"dashboard.html",context)
+
     return redirect("index")
 
